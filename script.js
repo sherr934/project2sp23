a8de16266a27ed49e9ca6524ea04aff3f444bb0a
sherr934 <sherr934@newschool.edu>
2023-03-27 12:58:18 -0400

    added files

---
+++ script.js
@@ -0,0 +1,222 @@
+let erasers = [
+    {
+        "name": "Dio",
+        "brand": "Tombow",
+        "height": 1,
+        "width": 2,
+        "length": 5,
+        "age": 2,
+        "usage": "erasing pencil sketches before inking",
+        "storage": "out on my table!!",
+        "img": "img/dio.png"
+    },
+    {
+        "name": "Disha",
+        "brand": "Staedtler",
+        "height": 0.8,
+        "width": 1.8,
+        "length": 5,
+        "age": 0.6,
+        "usage": "I actually haven’t used it much in a while. But if I were to specify it would be for sketching. ",
+        "storage": "In my pouch ",
+        "img": "img/disha.png"
+    },
+    {
+        "name": "Michelle",
+        "brand": "Tombow",
+        "height": 1,
+        "width": 2,
+        "length": 4.5,
+        "age": 1,
+        "usage": "I use my eraser to erase the memories that are just too embarrassing to keep in my journal.",
+        "storage": "In my desk drawer.",
+        "img": "img/michelle.png"
+    },
+    {
+        "name": "Kush",
+        "brand": "Staedtler",
+        "height": 1.1,
+        "width": 2.1,
+        "length": 6.2,
+        "age": 1,
+        "usage": "Random Doodles and Rough Sketches",
+        "storage": "On my Desk in an organiser",
+        "img": "img/kush.png"
+    },
+    {
+        "name": "Maria",
+        "brand": "Pentel",
+        "height": 1.1,
+        "width": 1.7,
+        "length": 4.4,
+        "age": 2,
+        "usage": "I don’t really use it because I tend to write with a pen, which is why it’s so new. I do break it out for the occasional crossword puzzle, and I most recently used it for my Machine Learning midterm. ",
+        "storage": "It rolls around the bottom of my tote :)",
+        "img": "img/maria.png"
+    },
+    {
+        "name": "Makena",
+        "brand": "Unknown",
+        "height": 2,
+        "width": 3,
+        "length": 2,
+        "age": 4,
+        "usage": "Whenever I draw with pencils",
+        "storage": "In my pencil case",
+        "img": "img/makena.png"
+    },
+    {
+        "name": "Nate",
+        "brand": "Staedtler",
+        "height": 1.25,
+        "width": 2.17,
+        "length": 6.28,
+        "age": 2,
+        "usage": "I’ve only used us a few times for orthographic and isometric drawings since I mostly use pens.",
+        "storage": "I keep it in a mug on my desk ",
+        "img": "img/nate.png"
+    },
+    {
+        "name": "Bar",
+        "brand": "Pentel",
+        "height": 1,
+        "width": 2.5,
+        "length": 5,
+        "age": 3,
+        "usage": "Product sketches, journaling, labeling wood pieces,",
+        "storage": "Pencil case ",
+        "img": "img/bar.png"
+    },
+        {
+        "name": "Laura ",
+        "brand": "Deli ",
+        "height": 2,
+        "width": 4.5,
+        "length": 3,
+        "age": 0,
+        "usage": "Drawing/sketching and writing in notebooks ",
+        "storage": "Pencil case ",
+        "img": "img/laura.png"
+    },
+    {
+        "name": "Adi",
+        "brand": "Staedtler",
+        "height": 0.8,
+        "width": 2.2,
+        "length": 5.3,
+        "age": 2,
+        "usage": "Big pieces of text that need erasing usually but sometimes smaller parts",
+        "storage": "Pencil case",
+        "img": "img/adi.png"
+    },
+    {
+        "name": "Scott",
+        "brand": "Sanford",
+        "height": 1,
+        "width": 2,
+        "length": 5.5,
+        "age": 43,
+        "usage": "I used to use it for grade school homework, then in “real life” when writing letters. I have definitely used it for sketching out ideas, especially before painting. ",
+        "storage": "A super early memory of my grandparents was them giving me and my brothers a small 15-drawer metal storage unit to put little things in. I e kept it all these years. The eraser lives faithfully in drawer 7 (second row; second column) along with paper clips, a pencil sharpener, a tiny pair of scissors and some mini safety pins. It will be there u til the day I die. ",
+        "img": "img/scott.png"
+    },
+    {
+        "name": "Fiore ",
+        "brand": "Staedtler",
+        "height": 1.2,
+        "width": 2.2,
+        "length": 1.1,
+        "age": 1,
+        "usage": "Man i never use erasers, i don’t even know where mine is. i usually useit when i’m doing a base sketch for a painting or drawing tho  ",
+        "storage": "AIDANS ROOM😈😈😈",
+        "img": "img/fiore.png"
+    },
+    {
+        "name": "Sam",
+        "brand": "Staedtler",
+        "height": 1.3,
+        "width": 2.2,
+        "length": 6.4,
+        "age": 1,
+        "usage": "Drawing",
+        "storage": "Sideboard drawer",
+        "img": "img/sam.png"
+    },
+    {
+        "name": "Simran",
+        "brand": "Pentel",
+        "height": 1.15,
+        "width": 2.3,
+        "length": 4.98,
+        "age": 2,
+        "usage": "School (whenever i fuck up in math problems, mostly on exams)",
+        "storage": "In pencil case",
+        "img": "img/simran.png"
+    },
+    {
+        "name": "Taya",
+        "brand": "Unknown",
+        "height": 1,
+        "width": 2,
+        "length": 5.4,
+        "age": 11,
+        "usage": "I truly don’t think I have actually used it in a decade, and even before that i dont think i used it much because it was bad quality. I just liked playing with the zipper case",
+        "storage": "In a my purple green striped falling apart pencil used that i used to use in high school ",
+        "img": "img/taya.png"
+    },
+    {
+        "name": "Cecelia",
+        "brand": "Staedtler",
+        "height": 0.25,
+        "width": 1,
+        "length": 2.5,
+        "age": 0,
+        "usage": "Drawing landscapes ",
+        "storage": "Art store",
+        "img": "img/cecelia.png"
+    },
+    {
+        "name": "Anna",
+        "brand": "Muji",
+        "height": 1,
+        "width": 2.5,
+        "length": 6,
+        "age": 6,
+        "usage": "I used it a lot for math and science work in highschool. I brought it to college despite the fact that I dont really use it much anymore, as i usually prefer to write in pen, but recently my roomate has been using it for her math homework",
+        "storage": "The top drawer of my desk",
+        "img": "img/anna.png"
+    }
+];
+
+function displayErasers(eraser) {
+  const eraserCollection = document.getElementById('collection');
+  const eraserDiv = document.createElement('div');
+  eraserDiv.classList.add('eraser', 'photo');
+  eraserDiv.innerHTML = `
+  <img class='eraserimg' src=${eraser.img}>
+  <p>__</p>
+  <p>owner: ${eraser.name}</p>
+  <p>brand: ${eraser.brand}</p>
+  <p>${eraser.height} x ${eraser.width} x ${eraser.length}"</p>
+  <p>age: ${eraser.age} years old</p>
+  <p>__</p>
+  `;
+  eraserCollection.appendChild(eraserDiv);
+
+  eraserDiv.addEventListener('click', () => {
+    if (eraserDiv.classList.contains('photo')) {
+      eraserDiv.classList.remove('photo');
+      eraserDiv.classList.add('info');
+    } else {
+      eraserDiv.classList.remove('info');
+      eraserDiv.classList.add('photo');
+    }
+  });
+}
+    if (document.body.style.lineHeight == "")
+    { 
+        document.body.style.lineHeight = ".1em";
+    }
+    document.body.style.lineHeight = parseFloat(document.body.style.lineHeight) + (0.4) + "em";
+
+erasers.forEach(displayErasers);
